ead93c418125ac4b81799be81278cc9bda571e26
Zoha Asghar <callmezoiii42@gmail.com>
2023-12-22 19:53:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// let userName = prompt("Please Enter your name to visit our website? ") 
+// window.onload = function(){
+//    let message ="Good Day"
+// //    let firstName = prompt("Please Enter Your First Name?")
+// //    let lastName = prompt("Please Enter Your Last Name?")
+// //    let fullName = firstName +" "+ lastName
+//    let banger ="!"
+//    document.getElementById("userName").innerHTML = message + " " + userName+ banger
+// }
+//---------------------Add Name in Storage---------------------//
+function addNameInStorage(){
+   let newName = prompt("Enter your name")
+   var names = localStorage.getItem('users')   // value ai gi string m
+     
+   if(names == null){
+      names = []
+   } else {
+      names = JSON.parse(names)  //string ko number->array m karnt ky laiy
+   }
+   // console.log(typeof names)
+   names.push(newName)
+   console.log(names)
+   localStorage.setItem('users', JSON.stringify(names))  //stringify = number ko string m karny  k laiy
+   output.innerHTML = newName + " have been stored in storage."
+}
+//---------------------Print Names from Storage---------------------//
+function printNamesFromStorage(){
+   var names = localStorage.getItem('users')
+   if(names == null){
+      alert('There is nothing to print')
+      return
+   } 
+   names = JSON.parse(names)
+   output.innerHTML = names
+}
+//---------------------Delete Names from Storage---------------------//
+function deleteNamesFromStorage(){
+   localStorage.removeItem('users')
+   output.innerHTML = "All Names have been deleted from Storage"
+}
+//show output
+function result(name){
+   return document.getElementById('output').innerHTML += name + "<br>"
+}
+//Clear output
+function clearOutputButton(){
+   document.getElementById('output').innerHTML = ""
+}
